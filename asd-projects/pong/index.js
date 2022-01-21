c49240b7316a7c57ee6150c5edf06d11ea7bfb16
AKelly39 <mechamind02201@gmail.com>
2022-01-21 21:38:19 +0000

    pong progress

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -17,17 +17,17 @@ function runProgram(){
   function gameObject (id, speedX, speedY) {
     var gameObject = {};
     gameObject.id = id;
-    gameObject.x = parseFloat($("#id").css("left"));
-    gameObject.y = parseFloat($("#id").css("top"));
+    gameObject.x = parseFloat($(id).css("left"));
+    gameObject.y = parseFloat($(id).css("top"));
     gameObject.speedX = speedX;
     gameObject.speedY = speedY;
-    gameObject.width = $("#id").width();
-    gameObject.height = $("#id").height();
+    gameObject.width = $(id).width();
+    gameObject.height = $(id).height();
     return gameObject;
   }
 
-  var paddleL = gameObject("#paddleL", 0, 0);
-  var paddleR = gameObject("#paddleR", 0, 0);
+  var paddleR = gameObject("#paddleL", 0, 0);
+  var paddleL = gameObject("#paddleR", 0, 0);
   var pongBall = gameObject("#pongBall", 0, 0);
 
 
@@ -55,33 +55,12 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {      
-    function moveObject(gameObject) {
-      gameObject.x += gameObject.speedX;
-      gameObject.y += gameObject.speedY;
-      $("#gameObject").css("left", gameObject.x);
-      $("#gameObject").css("top", gameObject.y);
-    }
+    moveObject(paddleL);
+    moveObject(paddleR);
+    moveObject(pongBall);
+    
 
-    function wallCollision(gameObject) {
-      if (gameObject.x < 0 || gameObject.y < 0 ||
-        gameObject.x + gameObject.width > BOARD_WIDTH ||
-        gameObject.y + gameObject.height > BOARD_HEIGHT) {
-          return true;
-      } 
-      if (pongBall.x < 0) {
-
-        $("#scorePlayerR").text(updatedScore)
-        startBall;
-
-      } if (pongBall.x + pongBall.width > BOARD_WIDTH) {
-
-        $("#scorePlayerL").text(updatedScore);
-        startBall;
-
-      } else {
-         return false;
-      }
-    }
+   
     
 
   }
@@ -99,32 +78,33 @@ function runProgram(){
 
   //My main issue here in pong is that I can't move my paddles
   function handleKeyDown(event) { 
+    console.log(event.which);
     if (event.which === KEY.UP) {
-      paddleR.speedY += 5;
+      paddleL.speedY = -5;
     }
     if (event.which === KEY.DOWN) {
-      paddleR.speedY -= 5;
+      paddleL.speedY = 5;
     }
     if (event.which === KEY.W) {
-      paddleL.speedY += 5;
+      paddleR.speedY = -5;
     }
      if (event.which === KEY.S) {
-      paddleL.speedY -= 5;
+      paddleR.speedY = 5;
     }
   }
 
   function handleKeyUp(event) {
     if (event.which === KEY.UP) {
-      paddleR.speedY = 0;
+      paddleL.speedY = 0;
     }
    if (event.which === KEY.DOWN) {
-      paddleR.speedY = 0;
+      paddleL.speedY = 0;
     }
     if (event.which === KEY.W) {
-      paddleL.speedY = 0;
+      paddleR.speedY = 0;
     }
     if (event.which === KEY.S) {
-      paddleL.speedY = 0;
+      paddleR.speedY = 0;
     }
 
   }
@@ -138,6 +118,33 @@ function runProgram(){
     pongBall.x = 100;
     pongBall.y = 200;
     pongBall.speedX = (Math.random() * 3 + 2) * (Math.random() > 0.5 ? -1 : 1);
+  }
+  function moveObject(gameObject) {
+    console.log(gameObject);
+    gameObject.x += gameObject.speedX;
+    gameObject.y += gameObject.speedY;
+    $(gameObject.id).css("left", gameObject.x);
+    $(gameObject.id).css("top", gameObject.y);
+  }
+  function wallCollision(gameObject) {
+    if (gameObject.x < 0 || gameObject.y < 0 ||
+      gameObject.x + gameObject.width > BOARD_WIDTH ||
+      gameObject.y + gameObject.height > BOARD_HEIGHT) {
+        return true;
+    } 
+    if (pongBall.x < 0) {
+
+      $("#scorePlayerR").text(updatedScore)
+      startBall;
+
+    } if (pongBall.x + pongBall.width > BOARD_WIDTH) {
+
+      $("#scorePlayerL").text(updatedScore);
+      startBall;
+
+    } else {
+       return false;
+    }
   }
 
 
